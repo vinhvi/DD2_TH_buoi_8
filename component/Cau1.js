783834a2a15858f8afa9bf09305461d0eeee1f4b
vinhvi <vithevinh123@gmail.com>
2022-10-27 10:17:49 +0700

    finish cau 1

---
+++ component/Cau1.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+import { Animated, Text, StyleSheet, SafeAreaView } from "react-native";
+
+const Cau1 = () => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+  }, []);
+  return (
+    <SafeAreaView>
+      <Animated.View style={[{ opacity: fadeAnim }]}>
+        <Text style={styles.fadingText}>Wellcome to Animation ReactNative</Text>
+      </Animated.View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  fadingText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
+export default Cau1;
